311bbeb12a7ac1328eb85ff28c679e37320352c4
Rafi Haidari <r.haidari@pomtech.com>
2021-11-13 16:46:45 +0430

    Refactor index file.

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
-
-
-function include_class($class_name)
+class Index
 {
-    $path = __DIR__ . '/include/' . $class_name . '.php';
-    if (file_exists($path)) {
-        require_once($path);
-    } else {
-        echo '(' . $class_name . ') File doesn\'t exist <br />' . PHP_EOL;
+    public static function include_class($class_name)
+    {
+        $path = __DIR__ . '/include/' . $class_name . '.php';
+        if (file_exists($path)) {
+            require_once($path);
+        } else {
+            echo '(' . $class_name . ') File doesn\'t exist <br />' . PHP_EOL;
+        }
     }
 }
 
-include_class('Config');
-
-
+$index = new Index;
+$index->include_class('Config');
